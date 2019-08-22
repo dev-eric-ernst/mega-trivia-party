a01b0bdf5006c5cc0a7689c69d503ce89f81b126
dev-eric-ernst <dev@eric-ernst.com>
2019-08-22 14:54:51 -0400

    update

--- server/server.js
+++ server/server.js
@@ -38,7 +38,14 @@ const candidates = {
 }
 
 const server = express()
-    .use(express.static('public'))
+    .use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        )
+        next()
+    })
     .use(allowCrossDomain)
     .get('/bb', (req, res) => res.json(candidates))
     .listen(PORT, () => console.log(`Listening on ${PORT}`))

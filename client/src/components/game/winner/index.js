11d85961cf273907a4b4c8321a84377bbe8250c3
dev-eric-ernst <dev@eric-ernst.com>
2019-09-19 11:56:13 -0400

    styled winner screen

--- client/src/components/game/winner/index.js
+++ client/src/components/game/winner/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './index.css'
 
 export default function Winner({ winner, loser }) {
     return (
-        <div>
-            <p>Winner: {winner.display}</p>
-            <p>Loser: {loser.display}</p>
-         </div>
+        <main className="winner">
+            <div className="winner-label">Winner is...</div>
+            <div className="winner-display">{winner.display}</div>
+            <div className="loser">Last Place: <span className="loser-display">{loser.display}</span></div>
+        </main>
     )
 }
